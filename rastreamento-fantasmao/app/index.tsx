d87a0ace38b1c3ee448cfb3eafc7191e18d85065
Cayo Cesar <cayocesar652@gmail.com>
2025-11-30 19:58:57 -0300

    Atualiza chave AIO_KEY e adiciona .gitignore para ignorar node_modules

--- rastreamento-fantasmao/app/index.tsx
+++ rastreamento-fantasmao/app/index.tsx
@@ -16,7 +16,7 @@ if (Platform.OS !== 'web') {
 
 // --- CONFIGURAÇÕES ---
 const AIO_USERNAME = "cayocesar652";
-const AIO_KEY = "aio_XsDA61XeMyCZsXdkNFIycqvpPPzB"; 
+const AIO_KEY = "aio_sAXn82ZYlpGVLSMIM4eyxNCkXOsG"; 
 const API_URL = `https://io.adafruit.com/api/v2/${AIO_USERNAME}/feeds`;
 
 export default function Index() {
